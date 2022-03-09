8a4365af607db873693910cf4ddab7f6dcfe1e6b
Philip Davis <philipcdavis11@gmail.com>
2022-03-08 23:08:05 -0800

    Initial Commit

---
+++ sceneview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sceneview
+//
+//  Created by Philip Davis on 3/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sceneview/sceneviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  sceneviewApp.swift
+//  sceneview
+//
+//  Created by Philip Davis on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct sceneviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
